40694f7af3d54a5234711b1b835661e6b7a85b97
amenilaagili <amenilaajili@gmail.com>
2021-03-20 17:31:16 +0100

    checkpointState

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import { Profil } from "./Profil/Profil";
+
+function App() {
+    return (
+        <div className="App">
+            <Profil />
+        </div>
+    );
+}
+
+export default App;
